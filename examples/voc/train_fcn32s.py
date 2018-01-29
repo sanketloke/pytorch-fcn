5fa19a770be97d9662928d892c51eb9185edf641
Sanket Lokegaonkar <sloke@vt.edu>
2018-01-28 19:21:01 -0500

    SP 4

--- examples/voc/train_fcn32s.py
+++ examples/voc/train_fcn32s.py
@@ -63,6 +63,7 @@ def get_parameters(model, bias=False):
         torchfcn.models.FCN32s,
         torchfcn.models.FCN16s,
         torchfcn.models.FCN8s,
+        FCN8s
     )
     for m in model.modules():
         if isinstance(m, nn.Conv2d):
