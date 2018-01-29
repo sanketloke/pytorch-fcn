981fe9ea9f43bf0dfcaf1ebeb1b7ec7ad5469e9b
Sanket Lokegaonkar <sloke@vt.edu>
2018-01-28 19:13:07 -0500

    SP 4

--- examples/voc/train_fcn8s_atonce.py
+++ examples/voc/train_fcn8s_atonce.py
@@ -48,6 +48,33 @@ configurations = {
 }
 
 
+
+
+
+
+def cross_entropy2d(input, target, weight=None, size_average=True):
+    # input: (n, c, h, w), target: (n, h, w)
+    n, c, h, w = input.size()
+    # log_p: (n, c, h, w)
+    if LooseVersion(torch.__version__) < LooseVersion('0.3'):
+        # ==0.2.X
+        log_p = F.log_softmax(input)
+    else:
+        # >=0.3
+        log_p = F.log_softmax(input, dim=1)
+    # log_p: (n*h*w, c)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous()
+    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    log_p = log_p.view(-1, c)
+    # target: (n*h*w,)
+    mask = target >= 0
+    target = target[mask]
+    loss = F.nll_loss(log_p, target, weight=weight, size_average=False)
+    if size_average:
+        loss /= mask.data.sum()
+    return loss
+
+
 here = osp.dirname(osp.abspath(__file__))
 
 class Trainer(object):
